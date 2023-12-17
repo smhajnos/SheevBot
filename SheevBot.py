c1c8f1955f2c875a60a4d364f3584ed9a5139e78
Sam Hajnos <smhajnos@gmail.com>
2023-12-17 10:02:03 -0500

    SequelMemes

--- SheevBot.py
+++ SheevBot.py
@@ -61,7 +61,7 @@ reddit = praw.Reddit(client_id=sheevsecrets.REDDIT_CLIENT_ID,
                      password=sheevsecrets.REDDIT_PASSWORD,
                      user_agent='SheevBot, the /r/PrequelMemes mod bot',
                      username=username)  
-subreddit_names = ["PrequelMemes", "PrequelMemesTest"]
+subreddit_names = ["PrequelMemes", "PrequelMemesTest","SequelMemes"]
 #subreddit_names = ["PrequelMemesTest"]
 subreddits = []
 for s in subreddit_names:
