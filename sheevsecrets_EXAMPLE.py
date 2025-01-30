0e396e0f0d30280b1b01ce61a4a2ee2289952dff
Sam Hajnos <smhajnos@gmail.com>
2025-01-29 21:22:53 -0500

    configurability

---
+++ sheevsecrets_EXAMPLE.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 14:12:04 2022
+
+@author: sam
+"""
+
+DISCORD_TOKEN="wharrgarbl"
+REDDIT_SECRET="imafirinmalaser"
+REDDIT_CLIENT_ID="iliketrains"
+REDDIT_PASSWORD="charlietheunicorn"
